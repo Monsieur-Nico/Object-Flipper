9b886bc177a6d761d157bd475025f881bdafe9ee
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-15 13:24:06 -0500

    Change index

--- dist/src/index.js
+++ dist/src/index.js
@@ -3,15 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv").config();
-const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
 const arrays_1 = __importDefault(require("./helpers/arrays"));
 const objects_1 = __importDefault(require("./helpers/objects"));
-const port = process.env.PORT || 8080;
-const app = (0, express_1.default)();
-app.use(express_1.default.static(path_1.default.join(__dirname, "public")));
-app.listen(port, () => {
-    console.log("listening on port " + port);
-});
 module.exports = { arrayToObject: arrays_1.default, objectToArray: objects_1.default };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,4 @@
-require("dotenv").config();
-import express from "express";
-import path from "path";
 import arrayToObject from "./helpers/arrays";
 import objectToArray from "./helpers/objects";
-
-const port: string | number = process.env.PORT || 8080;
-const app = express();
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.listen(port, () => {
-  console.log("listening on port " + port);
-});
 
 module.exports = { arrayToObject, objectToArray };
