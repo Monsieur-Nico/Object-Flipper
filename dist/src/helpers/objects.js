50d79ddecf7a9973fbb21b1f5154e2723e7ef92e
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-02-15 12:31:37 -0500

    Implement ObjectToArray

--- dist/src/helpers/objects.js
+++ dist/src/helpers/objects.js
@@ -1,9 +1,11 @@
 "use strict";
-// interface Result {
-//   []: {}
-// }
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * Turns an object of objects into an array of objects
  *
  * @param object An object of objects
  */
+function objectToArray(object) {
+    return Object.values(object);
+}
+exports.default = { objectToArray };
--- src/helpers/objects.ts
+++ src/helpers/objects.ts
@@ -1,9 +1,10 @@
-// interface Result {
-//   []: {}
-// }
-
 /**
  * Turns an object of objects into an array of objects
  *
  * @param object An object of objects
  */
+function objectToArray(object: {}): {}[] {
+  return Object.values(object);
+}
+
+export default { objectToArray };
